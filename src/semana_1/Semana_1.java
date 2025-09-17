adf2af7efa19eeb1a52b66eda2b3eb6516a19baf
jasonjosehernandezdelgado@gmail.com <jasonjosehernandezdelgado@gmail.com>
2025-09-16 21:55:03 -0600

    Se agrega el proyecto

---
+++ src/semana_1/Semana_1.java
@@ -0,0 +1,119 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana_1;
+//import javax.swing.JOptionPane;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author YAYO
+ */
+public class Semana_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        //Ejercicios de practica
+       /**
+        String cadena;
+        int entero;
+        double decimal;
+        char letra;
+        
+        cadena=JOptionPane.showInputDialog("Ingrese una cadena: ");
+        entero=Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero entero: "));
+        decimal=Double.parseDouble(JOptionPane.showInputDialog("Ingrese un numero decimal: "));
+        letra=JOptionPane.showInputDialog("Ingrese una letra: ").charAt(0);
+        JOptionPane.showMessageDialog(null, "La cadena es: "+cadena);
+        JOptionPane.showMessageDialog(null, "El numero es: "+entero);
+        JOptionPane.showMessageDialog(null, "El decimal es: "+decimal);
+        JOptionPane.showMessageDialog(null, "La letra es: "+letra);*/
+       
+       /**int num1,num2,suma,resta,mul;
+       double div,resto;
+        System.out.println("Digite 2 numeros: ");
+        num1=entrada.nextInt();
+        num2=entrada.nextInt();
+        suma=num1+num2;
+        resta=num1-num2;
+        mul=num1*num2;
+        div=num1/num2;
+        resto=num1%num2;
+        
+        System.out.println("La suma es: "+suma);
+        System.out.println("La resta es: "+resta);
+        System.out.println("La multiplicacion es: "+mul);
+        System.out.println("La division es: "+div);
+        System.out.println("El residuo es: "+resto);*/
+       
+       /**int nota1,nota2,nota3, suma;
+       
+        System.out.println("Digite 3 notas: ");
+        nota1=entrada.nextInt();
+        nota2=entrada.nextInt();
+        nota3=entrada.nextInt();
+        suma=nota1+nota2+nota3;
+        System.out.println("La suma de las 3 notas es: "+suma);*/
+        
+       /**double salario, semanal;
+       int horas;
+       
+        System.out.println("Ingrese su salario por hora: ");
+        salario=entrada.nextDouble();
+        System.out.println("Ingrese las horas trabajadas: ");
+        horas=entrada.nextInt();
+        semanal=salario*horas;
+        System.out.println("Su salario semanal fue de: "+semanal);*/
+        
+       /**double g,l,j,tot;
+       
+        System.out.println("Dolares de Guillermo: ");
+        g=entrada.nextDouble();
+        l=g/2;
+        j=(l+g)/2;
+        tot=l+j+g;
+        System.out.println("La cantidad de dinero que hay entre los 3 es de: "+tot);*/
+       
+       /**double comision,venta,tot;
+       int cantidad_carros,suma_carro;
+       
+       cantidad_carros=Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de carros vendidos: "));
+       suma_carro=Integer.parseInt(JOptionPane.showInputDialog("Ingrese la suma de dinero de carros vendidos: "));
+       comision=cantidad_carros*150;
+       venta=suma_carro*0.05;
+       tot=1000+comision+venta;
+       JOptionPane.showMessageDialog(null, "El salario final es de:$"+tot);*/
+       
+       /**int participacion, parcial1,parcial2,parcialfinal;
+       double nota;
+        
+        participacion=Integer.parseInt(JOptionPane.showInputDialog("Participacion: "));
+        parcial1=Integer.parseInt(JOptionPane.showInputDialog("Parcial 1: "));
+        parcial2=Integer.parseInt(JOptionPane.showInputDialog("Parcial 2: "));
+        parcialfinal=Integer.parseInt(JOptionPane.showInputDialog("Examen final: "));
+        
+        nota=(participacion*0.10)+(parcial1*0.25)+(parcial2*0.25)+(parcialfinal*0.40);
+        
+        JOptionPane.showMessageDialog(null, "Nota final; "+nota);*/
+       
+       /**int num1,num2;
+       double suma;
+       
+        System.out.println("Numero 1: ");
+        num1=entrada.nextInt();
+        System.out.println("Numero 2: ");
+        num2=entrada.nextInt();
+        suma=Math.pow(num1, 2)+2*num1*num2+Math.pow(num2, 2);
+        System.out.println("Resultado: "+suma);*/
+       
+       //-------------Primera clase de JAVA--------------
+       
+       
+       
+    }
+    
+}
